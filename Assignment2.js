de09c534992b52fbf8a5011be85d40a895438988
AbubakarAamir <153678759+AbubakarAamir@users.noreply.github.com>
2024-02-05 00:34:59 +0500

    Assignment number 2

---
+++ Assignment2.js
@@ -0,0 +1,191 @@
+
+
+                            // Assignment no # 3:
+//  Name : Abubakar Aamir
+
+
+
+
+                                // Question 1
+
+
+// var city =prompt("Enter your city name");
+//  if(city.toLowerCase()==="karachi"){
+//     alert("Welcome to the city of lights");
+//  }
+//  else{
+//     alert("Welcome to " + city);
+//  }
+ 
+
+
+
+                                   // Question 2
+
+    
+// var gender = prompt("Enter your gender");
+// if (gender .toLowerCase()==="male"){
+//     alert("Good morning  sir");
+// }
+// else if (gender .toLowerCase() ==="female" ){
+//     alert("Good morning Mam");
+// }
+// else{
+//     alert("Good morning");
+// }
+
+
+
+
+
+
+                                 // Question 3
+
+
+// var fuel = prompt("Enter the remaining fuel in your car in litres?");
+// if(fuel<0.25){
+//     alert("Please immediately  refill the fuel in your car");
+// }
+// else {
+//     alert("Refilling fuel is not important");
+// }
+
+
+                        // Question 4
+
+//part (a):
+// var a = 4;
+//       if (++a === 5){
+//        alert("given condition for variable a is true");
+//         } 
+ 
+//(a) Output : "given condition for variable a is true"
+
+//part(b): 
+//var b = 82;
+//if (b++ === 83){
+//alert("given condition for variable b is true");
+//  } 
+
+//(b) output: "no output"
+ 
+//part(c):
+
+//var c = 12;
+//if (c++ === 13){
+//alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// } 
+
+//part(c) output: "condition 2 is true"
+//                   "condition 4 is true"
+
+
+
+
+// part (d):
+//  var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// } 
+  
+//part (d) output:"The cost equals"
+
+
+//part (e):
+// if (true){
+//  alert("True");
+// } if (false){
+// alert("False");
+// } 
+ 
+//part (e) output : "True"
+
+// part (f):
+
+// if("car" < "cat"){
+// alert("car is smaller than cat");
+// } 
+
+//part (f) output: "car is smaller than cat"
+
+
+                       // Question 5
+
+
+// var secretNum = 7;
+// var user = +prompt("Guess the secret number (between 1 to 10): ");
+// if (user === secretNum) {
+//     alert("Great! its correct answer.");
+// } else if (user + 1 === secretNum || user - 1 === secretNum) {
+//     alert("Close enough to the correct answer.");
+// } else {
+//     alert("Sorry, incorrect guess")
+// }
+
+
+                      // Question 6
+// var user = +prompt("Enter a number: ");
+// if (user % 3 == 0) {
+//     alert(user + " is divisible by 3.");
+// } else {
+//     alert(user + " is not divisible by 3.");
+// }
+
+                      // Question 7
+// var user = +prompt("Enter a number: ");
+// if (user % 2 === 0) {
+//     alert(user + " is an even number.");
+// } else {
+//     alert(user + " is an odd number.");
+// }
+
+
+                       // Question 8
+// var temp = +prompt("Enter the temperature: ");
+// if (temp > 40) {
+//     alert("It is too hot outside.");
+// } else if (temp > 30) {
+//     alert("The Weather today is Normal.");
+// } else if (temp > 20) {
+//     alert("Today’s Weather is cool.");
+// } else if (temp > 10) {
+//     alert("OMG! Today’s weather is so Cool.");
+// } else {
+//     alert("It's very cold outside.");
+// }
+
+
+                    // Question 9
+// var num1 = +prompt("Enter the first number: ");
+// var num2 = +prompt("Enter the second number: ");
+// var perform = prompt("Enter the operation (+, -, *, /, %): ");
+// var result;
+// if (perform === "+") {
+//     result = num1 + num2;
+// } else if (perform === "-") {
+//     result = num1 - num2;
+// } else if (perform === "*") {
+//     result = num1 * num2;
+// } else if (perform === "/") {
+//     result = num1 / num2;
+// } else if (perform === "%") {
+//     result = num1 % num2;
+// } else {
+//     alert("Invalid operation");
+// }
+// alert("Result: " + result);
+
+
+
+
