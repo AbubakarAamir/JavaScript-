7cb4c40c154ac95a5fafbc1d282ba376a8391ffc
AbubakarAamir <153678759+AbubakarAamir@users.noreply.github.com>
2024-02-05 00:31:16 +0500

    Assignment number 3

---
+++ Assignment3.js
@@ -0,0 +1,73 @@
+
+// Question # 01
+// var a=+prompt("Enter a Value for a : ");
+// console.log("The Value of a is : " + a);
+// console.log("The Value of ++a is : " + (++a));
+// console.log("Now the Value of a is : " + a);
+// console.log("The Value of a++ is : " + (a++));
+// console.log("Now the Value of a is : " + a);
+// console.log("The Value of --a is : " + (--a));
+// console.log("Now the Value of a is : " + a);
+// console.log("The Value of a-- is : " + (a--));
+// console.log("Now the Value of a is : " + a);
+
+// Question # 02
+// var a = 2, b = 1;
+//  var result = --a ; 
+// console.log("The Value of --a at this stage is : " + a);
+// var subt = --a - --b ;
+// console.log("The Value of --a - --b at this stage is = "+ subt );
+//  var addi= --b + ++b ;
+//  console.log("The Value of --b + ++b at this stage is : " + addi);
+// var addi2 = ++b + b--;
+// console.log("The Value of ++b + b-- at this stage is : " + addi2);
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// console.log("The result is " + result);
+
+// Question # 03
+// var name = prompt("Enter Your Name : ");
+// console.log("Welcome "+ name +" to our website ");
+
+// Question # 04
+// var num = +prompt("Enter a number to show its mult table");
+
+// if (num != 0){
+//     var i;
+//     for (i=1 ; i<=10 ;i++){
+//         console.log(num +" x " + i + " = " + num * i);
+//     }  
+// }
+// else if (num == 0) {
+//     var i , a=5;
+//     for (i=1 ; i<=10 ;i++){
+//         console.log(a +" x " + i + " = " + a * i);
+//     }  
+// }
+
+// Question # 05
+// part (a)
+//  var subj1 = prompt("Enter subject 1 name : ");
+//   var subj2 = prompt("Enter subject 2 name : ");
+//    var subj3 = prompt("Enter subject 3 name : ");
+// part (b)
+// var tm = 100;
+// part (c)
+//  var marks1 = +prompt("Enter subject 1 marks : ");
+// part (d)
+//   var marks2 = +prompt("Enter subject 2 marks : ");
+//    var marks3 = +prompt("Enter subject 3 marks : ");
+
+//part (e)
+// var totalMarks = marks1 + marks2 + marks3;
+// var totalSubjects = 3;
+// var percentage = (totalMarks / (tm * totalSubjects)) * 100;
+// var resultMessage =(
+//   "Subject\t\tTotal Marks\tObtained Marks\n" +
+//   subj1 + "\t         " + tm + "\t      " + marks1 + "\n" +
+//   subj2 + "\t         " + tm + "\t       " + marks2 + "\n" +
+//   subj3 + "\t         " + tm + "\t        " + marks3 + "\n" +
+//   "Total\t\t     " + totalMarks + "\n" +
+//   "Percentage\t\t   " + percentage + "%");
+// alert(resultMessage);
+// console.log(resultMessage);
