c571fa63f0727b90bb9b8994253a936cb2614a12
AbubakarAamir <153678759+AbubakarAamir@users.noreply.github.com>
2024-02-18 02:18:26 +0500

    Javascript assignment

---
+++ Assignment 1 .js
@@ -0,0 +1,179 @@
+            // JAVA SRCIPT PRACTISE ASSIGNMENT 1
+// Q.1 - Write a Script to greet your website visitor using JS alert box. (Welcome to Our Website)
+
+    //                          Answer
+    // alert("welcome to our website")
+
+    // / Q.2 - Write a script to display message on your web page. (Error! Please Enter a valid password)
+
+    // //                          Answer
+    // console.log("please enter a valid password");
+
+    // Q.3 - Write a script to display message on your website:(Hint: Use line break)
+                              
+                                    //   Answer
+
+ //    console.log("welcome to our website\nScroll down to see Our Website'")
+// Q.4 - Write a script to display message in sequence(3 Alerts)
+    //                          Answer
+
+    // alert('This is a First Alert Message')
+    // alert('This is a Second Alert Message')
+    // alert('This is a Third Alert Message')
+
+
+// Q.5 - Make use of alerts in your  new/existing HTML & CSS Project
+    //                          Answer
+
+
+// Q.6 - Practice placement of element in following sections of your project in exercise 5:
+    // a. Head
+    // b.Body(before your page's HTML)
+    // c. Body(inside your page's HTML)
+    // d. Body(after your page's HTML)
+        //                          Answer
+
+
+
+// Q.7 - Declare a variable called username
+    //                          Answer
+
+    // var username
+
+
+// Q.8 - Declare a variable called myNAme & assign to it a string that represents your Full Name
+    //                          Answer
+
+    // var myName = 'Sheheryaar Ansar'
+
+
+// Q.9 - Write a script to
+    // a. Declare a JS variable, titled message.
+    // b. Assign "Hello World" to variable message.
+    // c. Display the message in alert box.
+        //                          Answer
+
+        // var titledMessage = 'Hello World'
+        // alert(titledMessage)
+
+
+// Q.10 - Write a script to save student's bio data in JS variables and show the data in alert boxes.
+    //                          Answer
+
+    // var bioData1 = 'Bio-Data(1)'
+    // var bioData2 = 'Bio-Data(2)'
+    // var bioData3 = 'Bio-Data(3)'
+    // var bioData4 = 'Bio-Data(4)'
+    // alert(bioData1)
+    // alert(bioData2)
+    // alert(bioData3)
+    // alert(bioData4)
+
+
+// Q.11 - Declare a variable called email and assign to it a string that represents your Email Address (e.g example@example.com). Show the below mentioned message in an alert box.(Hint: use string concatenation)
+    //                          Answer
+
+    // var email = 'Sheryaarlong@gmail.com'
+    // alert('Email: '+ email)
+
+
+// Q.12 - Write a script to display this in browser through JS.
+    //                          Answer
+
+    // See Q.13 for the Answer
+
+
+// Q.13 - Store following string in a variable and show in alert and browser through JS “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
+    //                          Answer
+
+    // var string = '▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬'
+    // alert(string)
+    // Answer No.12
+    // console.log(string)
+
+
+// Q.14 - Declare a variable called age & assign to it your age. Show your age in an alert box.
+    //                          Answer
+
+    // var age = 21
+    // alert('Age: '+age)
+
+
+// Q.15 - Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: “You have visited this site N times”.
+    //                          Answer
+
+    // var visit = 0
+    // alert('You Have Visited this Site ' + visit++ + ' Number of Times.')
+
+
+// Q.16 - Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+    //                          Answer
+
+    // var birthYear = 2002
+    // console.log('Birth Year: '+birthYear)
+
+
+// Q.17 - A visitor visits an online clothing store www.xyzClothing.com . Write a script to store in variables the following information: a. Visitor’s name b. Product title c. Quantity i.e. how many products a visitor wants to order Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+    //                          Answer
+
+    // var Name = 'Harry',
+    // product = 'Cold Drink',
+    // quantity = '10',
+    // store = 'ABZ Shopping Mall'
+    // alert(Name+' Purchase '+quantity + product+' from '+store)
+
+
+// Q.18 - Declare 3 variables in one statement
+    //                          Answer
+
+    // var myFullName = 'Sheheryaar Ansar', myCourse = 'Web Development #1', myLocation = 'Bano Qabil 2.0-'
+
+
+// Q.19 - Declare 5 legal & 5 illegal variable names
+    //                          Answer
+
+
+// Q.20 - Display this in your browser
+    // a) A heading stating “Rules for naming JS variables”
+    //b) Variable names can only contain ______, ______, ______ and ______. For example $my_1stVariable
+    //c) Variables must begin with a ______, ______ or _____. For example $name, _name or name
+    // d) Variable names are case _________
+    // e) Variable names should not be JS _________
+    //                          Answer
+
+
+// Q.21 - Write a program that take two numbers & add them in a new variable. Show the result in your browser
+    //                          Answer
+
+    // var program_1 = 10
+    // var program_2 = 5
+    // var program = program_1 + program_2
+    // alert('Sum of two Numbers: '+program)
+
+
+// Q.22 - Repeat task21 for subtraction, multiplication, division & modulus.
+    //                          Answer
+
+    // For Substraction
+    // var program_1 = 10
+    // var program_2 = 5
+    // var program = program_1 - program_2
+    // alert('Substraction of two Numbers: '+program)
+
+    // // For Multiplication
+    // var program_1 = 10
+    // var program_2 = 5
+    // var program = program_1 * program_2
+    // alert('Multiplication of two Numbers: '+program)
+
+    // // For Division
+    // var program_1 = 10
+    // var program_2 = 5
+    // var program = program_1 / program_2
+    // alert('Division of two Numbers: '+program)
+
+    // // For Modulus
+    // var program_1 = 10
+    // var program_2 = 5
+    // var program = program_1 % program_2
+    // alert('Modulus of two Numbers: '+,-)
